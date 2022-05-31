eafe7cc28fddd616745e1c1438ebc13ece3cc44e
Ahmed  Shite <ashite@e2r12p5.1337.ma>
2022-05-31 17:19:26 +0100

    lol

--- location.cpp
+++ location.cpp
@@ -18,58 +18,6 @@ void Location::set_path(std::string path)
     _path = path;
 }
 
-std::vector<std::string> Location::get_index()
-{
-    return _index;
-}
-
-void Location::set_index(std::vector<std::string> index)
-{
-    _index = index;
-}
-
-bool Location::get_auto_index()
-{
-    return _auto_index;
-}
-
-void Location::set_auto_index(bool autoIndex)
-{
-    _auto_index = autoIndex;
-}
-
-std::string Location::get_error_map(int error)
-{
-    return _error_map[error];
-}
-
-void Location::set_error_map(int error, std::string error_message)
-{
-    _error_map[error] = error_message;
-    //use this if you want to add more error messages
-    //_error_map.insert(std::pair<int, std::string>(error, error_message));
-    //_error_map.insert({error, error_message});
-}
-
-std::string Location::get_upload_directory()
-{
-    return _upload_directory;
-}
-
-void Location::set_upload_directory(std::string upload_directory)
-{
-    _upload_directory = upload_directory;
-}
-
-std::vector<std::string> Location::get_allowed_methods()
-{
-    return _allowed_methods;
-}
-
-void Location::set_allowed_methods(std::vector<std::string> allowed_methods)
-{
-    _allowed_methods = allowed_methods;
-}
 
 std::string Location::get_cgi_path()
 {
@@ -86,14 +34,7 @@ std::vector<std::string> Location::get_cgi_ext()
     return _cgi_ext;
 }
 
-//methods
-void Location::add_index(std::string index)
+void set_cgi_ext(std::vector<std::string> cgi_ext)
 {
-    _index.push_back(index);
+    _cgi_ext = cgi_ext;
 }
-
-void Location::add_allowed_method(std::string method)
-{
-    _allowed_methods.push_back(method);
-}
-
--- location.hpp
+++ location.hpp
@@ -10,9 +10,6 @@ class Location : public Server
 {
     private:
         std::string _path;
-        
-        
-        std::vector<std::string> _allowed_methods;
         std::string _cgi_path;
         std::vector<std::string> _cgi_ext;
         
@@ -24,23 +21,10 @@ class Location : public Server
         //geters and seters
         std::string get_path();
         void set_path(std::string path);
-        std::vector<std::string> get_index();
-        void set_index(std::vector<std::string> index);
-        bool get_auto_index();
-        void set_auto_index(bool auto_index);
-        std::string get_error_map(int error);
-        void set_error_map(int error, std::string error_message);
-        std::string get_upload_directory();
-        void set_upload_directory(std::string upload_directory);
-        std::vector<std::string> get_allowed_methods();
-        void set_allowed_methods(std::vector<std::string> allowed_methods);
         std::string get_cgi_path();
         void set_cgi_path(std::string cgi_path);
         std::vector<std::string> get_cgi_ext();
         void set_cgi_ext(std::vector<std::string> cgi_ext);
-        //methods
-        void add_index(std::string index);
-        void add_allowed_method(std::string method);
         void add_cgi_ext(std::string ext);
 };
  
